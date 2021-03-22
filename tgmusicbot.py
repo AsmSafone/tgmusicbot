8d7677d21062a3a1be34f9b400b4da80f4c256e3
Mr. SAFONE <77989182+AsmSafone@users.noreply.github.com>
2021-03-22 20:40:03 +0600

    Update tgmusicbot.py

--- tgmusicbot.py
+++ tgmusicbot.py
@@ -199,8 +199,7 @@ async def _upload_audio(message: Message, info_dict, audio_file):
     make_squarethumb(thumbnail_file, squarethumb_file)
     webpage_url = info_dict['webpage_url']
     title = info_dict['title']
-    caption = f"<b>✣ Uploaded By: 𝚂𝚊𝚏𝚘𝚗𝚎 𝙼𝚞𝚜𝚒𝚌 𝙱𝚘𝚝🎵 
-    ✣ Music: <a href=\"{webpage_url}\">{title}</a></b>"
+    caption = f"<b>✣ Uploaded Music: <a href=\"{webpage_url}\">{title}</a></b>"
     duration = int(float(info_dict['duration']))
     performer = info_dict['uploader']
     await message.reply_audio(audio_file,
